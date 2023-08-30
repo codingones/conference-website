adade5550c830608091bb153bdf43d15de5d261e
Marc Gavanier <marc.gavanier@gmail.com>
2023-08-30 17:34:26 +0200

    fix: set relative path for images (#8)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-console.log(process.env.NEXT_PUBLIC_BASE_PATH);
-console.log(process.env.NEXT_PUBLIC_ASSET_PREFIX);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ?? '',
   output: 'export',
   distDir: 'build',
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
