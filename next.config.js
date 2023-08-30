d2dfa41e23132cf7be9e32edcf7b7189e3d20d88
Marc Gavanier <marc.gavanier@gmail.com>
2023-08-30 14:37:58 +0200

    fix: use env variable to configure basePath and assetPrefix (#3)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
   output: 'export',
+  distDir: 'build',
   images: { unoptimized: true }
 };
 
