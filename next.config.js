01867cd3e5b5a52ea9da66b33e2e6197b95b96c7
Marc Gavanier <marc.gavanier@gmail.com>
2023-08-30 16:15:47 +0200

    fix: lig env in build (#7)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+console.log(process.env.NEXT_PUBLIC_BASE_PATH);
+console.log(process.env.NEXT_PUBLIC_ASSET_PREFIX);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ?? '',
   output: 'export',
   distDir: 'build',
   images: { unoptimized: true }
